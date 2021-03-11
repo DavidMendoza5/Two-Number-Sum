d0ccd48db35926dada0ea49218b00f7e58ed1217
David <cruz-azul99@hotmail.com>
2021-03-10 21:49:36 -0600

    Algorithm added

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 function twoNumberSum(array, targetSum) {
 	let sum = 0
-	for(let data of array){
-		sum += data
+	let index = 0
+	let second_index = index+1
+
+	console.log(array)
+
+	while(index < array.length) {
+		sum = array[second_index] + array[index]
+		if(sum === targetSum) {
+			let new_array = [array[index], array[second_index]]
+			return new_array
+		} else {
+			second_index++
+			if(second_index === array.length-1) {
+				index++
+				second_index = index+1
+			}
+		}
 	}
-	
-  // Write your code here.
+
 }
 
 // Do not edit the line below.
